14a71b8112230e96ae2ad60bc9117705ddc9977b
Elvis Serrão <elvis.serrao1@gmail.com>
2023-01-21 13:14:39 -0300

    Fix home controller spec

--- spec/requests/api/v1/home_spec.rb
+++ spec/requests/api/v1/home_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Api::V1::Home", type: :request do
   describe "GET /api/v1/home" do
     it "Get response with status :success" do
-      get root_path
+      get root_path, as: :json
 
       expect(response.content_type).to eq("application/json; charset=utf-8")
       expect(response).to have_http_status(:success)
