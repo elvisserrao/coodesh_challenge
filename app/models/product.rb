e9d50cfe9ad738401fcdc055174e9e2520fd45b8
Elvis Serrão <elvis.serrao1@gmail.com>
2023-01-21 17:08:32 -0300

    Run rubocop autofix

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-  enum :status, { draft: 0, published: 1, trash: 2}
+  enum :status, { draft: 0, published: 1, trash: 2 }
 
-  validates_presence_of :imported_t, :status
+  validates :imported_t, :status, presence: true
 end
