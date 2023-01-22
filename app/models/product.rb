d28d3f6e395beaf51912f0bdf094c8c57e164d99
Elvis Serrão <elvis.serrao1@gmail.com>
2023-01-22 18:25:37 -0300

    Set default values for status and imported_t before product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  before_validation :set_default_value
+
   enum :status, { draft: 0, published: 1, trash: 2 }
 
   validates :imported_t, :status, presence: true
+  private
+
+  def set_default_value
+    return if persisted?
+
+    self.imported_t = Time.zone.now
+    self.status = :draft
+  end
 end
