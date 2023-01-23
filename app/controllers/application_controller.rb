d3c4d11f3b3ef77d54a5e4870577c66309877d3f
Elvis Serrão <elvis.serrao1@gmail.com>
2023-01-22 21:01:25 -0300

    Ensure that all requests has api-key header with correct token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::API
   include Pagy::Backend
+
+  before_action :authenticate_request!
+
+  def authenticate_request!
+    return if request.headers['api-key'] == ENV['API_KEY']
+
+    render json: 'Please check your api key', status: :unauthorized
+  end
 end
