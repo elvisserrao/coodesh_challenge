2692e9d6a327d5444551748f47d0a05e08f284e1
Elvis Serrão <elvis.serrao1@gmail.com>
2023-01-23 09:35:14 -0300

    Fix controller path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :products, param: :code, except: [:create]
+  resources :products, controller: 'api/v1/products', param: :code, except: [:create]
   root "api/v1/home#welcome"
 end
